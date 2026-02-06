acc6b8beb98adaa84101909bcc282e0212f3d946
Alan Yu <alanrain205@gmail.com>
2026-02-05 23:15:58 -0700

    firebase built

--- ListyCity/app/build.gradle.kts
+++ ListyCity/app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
-
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -33,7 +33,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
@@ -42,4 +41,7 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 
+    implementation(platform("com.google.firebase:firebase-bom:34.8.0"))
+    implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-analytics")
 }
